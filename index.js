13a9c1c4efff967ad0d0ded56f8cd556216cf133
Lets-Fish <153985448+Lets-Fish@users.noreply.github.com>
2025-08-30 15:25:52 -0300

    outerhtml

--- index.js
+++ index.js
@@ -6,11 +6,24 @@ loadGame();
 setInterval(loadGame(),5000);
 //{}
 //Debug
-function addToHtmlID(id,str){
+function addAfterHtmlID(id,str){
     const element = document.getElementById(id);
+    element.outerHTML=element.outerHTML+str;
+}
+function addAfterHtmlName(name,str){
+    const element = document.getElementsByClassName(name);
     element.innerHTML=element.innerHTML+str;
 }
-function addToHtmlName(name,str){
+function addInHtmlID(id,str){
+    const element = document.getElementById(id);
+    var end = element.outerHTML.indexOf(" ");
+    if(element.outerHTML.indexOf(">")<end){
+        end = element.outerHTML.indexOf(">");
+    }
+    const className = element.outerHTML.substring(1,element.outerHTML.indexOf(">"));
+    element.innerHTML=element.outerHTML.substring(0,)+str;
+}
+function addInHtmlName(name,str){
     const element = document.getElementsByClassName(name);
     element.innerHTML=element.innerHTML+str;
 }
